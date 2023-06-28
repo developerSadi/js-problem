10322146d60caa2ca03ca98736cf4a010eda9e57
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-28 07:20:15 -0700

    add to swapjs

---
+++ swap.js
@@ -0,0 +1,17 @@
+// var a = 5;
+// var b = 7;
+// console.log("Before swap", "A=",a , "B =",b);
+
+// var temp = a;
+// a =b;
+// b = temp;
+// console.log("Aftere swap", "A=",a , "B =",b);
+
+var a = 10;
+var b = 15;
+console.log("Before Swap", "A=",a, "B=",b);
+
+var temp = a;
+a =b;
+b = temp;
+console.log("Aftere Swap", "A=",a, "B=", b);
