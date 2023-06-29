769daebd2fac65ee6bfda7147c8ac9df06eabffe
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-29 06:55:54 -0700

    Add to swap

--- swap.js
+++ swap.js
@@ -7,11 +7,19 @@
 // b = temp;
 // console.log("Aftere swap", "A=",a , "B =",b);
 
-var a = 10;
-var b = 15;
-console.log("Before Swap", "A=",a, "B=",b);
+// var a = 10;
+// var b = 15;
+// console.log("Before Swap", "A=",a, "B=",b);
 
-var temp = a;
-a =b;
-b = temp;
-console.log("Aftere Swap", "A=",a, "B=", b);
+// var temp = a;
+// a =b;
+// b = temp;
+// console.log("Aftere Swap", "A=",a, "B=", b);
+
+var x = 10;
+var y = 15;
+
+x = x + y;
+y = x - y;
+x = x - y;
+console.log("After Swap :","X=", x, "Y=", y);
