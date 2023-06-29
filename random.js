e0cff63bdce88e9077ecf6cfcced12197db05b23
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-29 07:47:11 -0700

    add to random number

---
+++ random.js
@@ -0,0 +1,14 @@
+// var num = 2.4999;
+// var result = Math.floor(num);// result asbe = 2;
+// var result2 = Math.ceil(num);// result asbe = 3;
+// var result3 = Math.round(num);// result asbe = 2.4999 result Hobe = 2; r jode 2.50 tahole 3 hobe.;
+// var random = Math.random();
+// var randomNumber = Math.random() * 6;
+// var output = Math.round(randomNumber);
+// console.log(output);
+
+for(i = 0; i<2; i++){
+    var randomNumber = Math.random() * 50;
+    var output = Math.round(randomNumber);
+    console.log(output);
+}
