dc44c0e2d667d6cb7b9944da52b297dd625f3c5e
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-29 22:15:42 -0700

    Add to Max number

---
+++ max.js
@@ -0,0 +1,26 @@
+// var business = 560;
+// var minister = 570;
+// var sosib = 780;
+// if(business > minister){
+//     if(business > sosib){
+//         console.log("Business Is a Big");
+//     }
+//     else{
+//         console.log("Sosib Is a Big");
+//     }
+// }
+// else{
+//     if(minister > sosib){
+//         console.log("Minister Is a Big");
+//     }
+//     else{
+//         console.log("Sosib Is A Big");
+//     }
+// }
+
+var business = 580;
+var minister = 680;
+var sosib = 1050;
+
+var max = Math.max(business,minister,sosib);
+console.log(max);
