3d87df4b81230db0973e0efd8d1cc0c4534656ae
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-06-29 22:48:01 -0700

    add to arrayMax number

---
+++ arrayMax.js
@@ -0,0 +1,20 @@
+// var marks = [45,78,63,80,95,65,32,44,];
+// var max = marks[0];
+
+// for(var i = 0; i < marks.length; i++){
+//     var element = marks[i];
+//     if(element > max){
+//         max  = element;
+//     }
+
+// }
+// console.log(max);
+var marks = [10,50,40,70,80,60,90];
+var max = marks[0];
+for(var i = 0; i < marks.length; i++){
+    var element = marks[i];
+    if(element > max){
+        max = element;
+    }
+}
+console.log(max);
