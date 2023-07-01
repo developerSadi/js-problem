ca635801a052f0fab9fe46e119424e53b4a8f2ce
Sekh Sadi <freelancersekhsadi@gmail.com>
2023-07-01 05:36:48 -0700

    add to word count

---
+++ wordCount.js
@@ -0,0 +1,23 @@
+// var speach = "I am a good person. I don't snore at night.";
+
+// var count = 0;
+// for(var i = 0; i < speach.length; i++){
+//     var char = speach[i];
+//     if(char == " "){
+//         count++;
+//     }
+  
+// }
+// count++;
+// console.log(count);
+
+var speech = "I am a freelancer sekh sadi.";
+var count = 0;
+for(var i=0; i < speech.length; i++){
+    var char = speech[i];
+    if(char == " "){
+        count++;
+    }
+}
+count++;
+console.log(count);
